f5ae40dd6b0ed2c0bdab691158c8013dce59372f
BabelDev0 <leonardo.straccali@gmail.com>
2022-06-13 11:31:32 +0200

    fixed: tt sample time for signal disturbances

--- c2.m
+++ c2.m
@@ -301,7 +301,7 @@ hold on;
 legend(Legend_arg);
 
 % STOP qui per sistema con controllore dinamico + specifiche
-if 1
+if 0
     return;
 end
 
@@ -315,7 +315,7 @@ FF = LL / (1 + LL);
 
 % tt potrebbe essere suddiviso in più intervalli di cambionamento a seconda
 % del caso di utilizzo, per comodità ne scegliamo uno unico
-tt = (0:1e1:1e3)'; 
+tt = (0:1e-4:1e1)'; 
 
 %% Check prestazioni in anello chiuso
 %% richiesta: w(t) = 0.75 * 1(t)
